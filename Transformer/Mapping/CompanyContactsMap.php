af09ffa7a87707b4f35b7d2d54f571f70508d268
Maksim Khortov <xoptov@mail.ru>
2015-03-18 18:10:25 +0300

    CRM-440 added API for company contacts

---
+++ Transformer/Mapping/CompanyContactsMap.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Perfico\CRMBundle\Transformer\Mapping;
+
+use Perfico\CRMBundle\Transformer\Converter\ClientsListChannelConverter;
+use Perfico\CRMBundle\Transformer\Converter\ClientsListUserConverter;
+use Perfico\CRMBundle\Transformer\Converter\PhoneConverter;
+use Perfico\CRMBundle\Transformer\Converter\DateConverter;
+
+class CompanyContactsMap implements MapInterface
+{
+    public function getReverseMap() {}
+
+    public function getMap()
+    {
+        return [
+            'id' => 'getId',
+            'email' => 'getEmail',
+            'firstName' => 'getFirstName',
+            'lastName' => 'getLastName',
+            'middleName' => 'getMiddleName',
+            'createdAt' => [
+                'converter' => new DateConverter(),
+                'method' => 'getCreatedAt'
+            ],
+            'channel' => [
+                'converter' => new ClientsListChannelConverter(),
+                'method' => 'getChannel'
+            ],
+            'dealStatuses' => [
+                'converter' => 'perfico_crm.api.clients_list_statuses_converter',
+                'method' => 'getDeals'
+            ],
+            'phones' => [
+                'converter' => new PhoneConverter(),
+                'method' => 'getPhones',
+                'collection' => true
+            ],
+            'user' => [
+                'converter' => new ClientsListUserConverter(),
+                'method' => 'getUser'
+            ]
+        ];
+    }
+} 
