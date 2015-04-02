541034e5a9e48b2b60a315d9a43c45e3d32db4ed
Maksim Khortov <xoptov@mail.ru>
2015-04-02 15:46:41 +0300

    RI-117 calendar search company

---
+++ Search/SearchConditionInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xoptov
+ * Date: 02.04.15
+ * Time: 13:52
+ */
+
+namespace Perfico\CRMBundle\Search;
+
+
+interface SearchConditionInterface {
+
+} 
---
+++ Service/Manager/SearchManagerInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xoptov
+ * Date: 02.04.15
+ * Time: 13:51
+ */
+
+namespace Perfico\CRMBundle\Service\Manager;
+
+
+interface SearchManagerInterface {
+
+} 
